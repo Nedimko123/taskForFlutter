479ef85f05cf96f82fea52584ca838e3e7e45dfc
Nedimko123 <nedimlav@gmail.com>
2022-04-01 09:44:31 +0200

    second commit

--- lib/screens/editors/noteeditor.dart
+++ lib/screens/editors/noteeditor.dart
@@ -93,92 +93,104 @@ class _NoteEditorState extends State<NoteEditor> {
               future: selectorbyId(index: indexfornote),
               builder: (context, snapshot) {
                 return Center(
-                  child: Column(
-                    children: [
-                      TextField(
-                        onChanged: (value) {},
-                        controller: _textEditingController,
-                        decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Input new name'),
-                      ),
-                      //Big problems here
-                      Text('Select category'),
-                      FutureBuilder(
-                          future: listofCategoriestherightway,
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.done) {
-                              if (snapshot.hasData) {
-                                return DropdownButton(
-                                  value: selectedCategoryExport,
-                                  items: categoryList
-                                      .map((item) => DropdownMenuItem(
-                                          value: item,
-                                          child: Text(
-                                            item,
-                                            style: TextStyle(fontSize: 18),
-                                          )))
-                                      .toList(),
-                                  onChanged: (item) => setState(() {
-                                    selectedCategoryExport = item.toString();
-                                    selectedCategory = selectedCategoryExport;
-                                    print(selectedCategory);
+                  child: Consumer(builder: ((context, ref, child) {
+                    AsyncValue<List> thisNote =
+                        ref.watch(noteThatNeedsToBeEdited);
+                    return thisNote.when(
+                        data: ((data) {
+                          return Column(
+                            children: [
+                              TextField(
+                                onChanged: (value) {},
+                                controller: _textEditingController,
+                                decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Input new name'),
+                              ),
+                              //Big problems here
+                              Text('Select category'),
+                              FutureBuilder(
+                                  future: listofCategoriestherightway,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.done) {
+                                      if (snapshot.hasData) {
+                                        return DropdownButton(
+                                          value: data.first['category'],
+                                          items: categoryList
+                                              .map((item) => DropdownMenuItem(
+                                                  value: item,
+                                                  child: Text(
+                                                    item,
+                                                    style:
+                                                        TextStyle(fontSize: 18),
+                                                  )))
+                                              .toList(),
+                                          onChanged: (item) => setState(() {
+                                            selectedCategoryExport =
+                                                item.toString();
+                                            selectedCategory =
+                                                selectedCategoryExport;
+                                            print(selectedCategory);
+                                          }),
+                                        );
+                                      } else {
+                                        return const Text('Nothing here');
+                                      }
+                                    } else {
+                                      return const CircularProgressIndicator();
+                                    }
                                   }),
-                                );
-                              } else {
-                                return const Text('Nothing here');
-                              }
-                            } else {
-                              return const CircularProgressIndicator();
-                            }
-                          }),
-                      Text('Select importance'),
-                      DropdownButton(
-                        value: selectedImportance,
-                        items: importance
-                            .map((item) => DropdownMenuItem<int>(
-                                value: item,
-                                child: Text(
-                                  item.toString(),
-                                  style: TextStyle(fontSize: 18),
-                                )))
-                            .toList(),
-                        onChanged: (item) => setState(() {
-                          selectedImportance = item as int;
+                              Text('Select importance'),
+                              DropdownButton(
+                                value: data.first['importance'],
+                                items: importance
+                                    .map((item) => DropdownMenuItem<int>(
+                                        value: item,
+                                        child: Text(
+                                          item.toString(),
+                                          style: TextStyle(fontSize: 18),
+                                        )))
+                                    .toList(),
+                                onChanged: (item) => setState(() {
+                                  selectedImportance = item as int;
 
-                          print(selectedImportance);
+                                  print(selectedImportance);
+                                }),
+                              ),
+                              Text('Select date and time'),
+                              DateTimePicker(
+                                type: DateTimePickerType.dateTimeSeparate,
+                                dateMask: 'd MMM, yyyy',
+                                initialValue: data.first['dates'],
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2100),
+                                icon: Icon(Icons.event),
+                                dateLabelText: 'Date',
+                                timeLabelText: "Hour",
+                                selectableDayPredicate: (date) {
+                                  // Disable weekend days to select from the calendar
+                                  if (date.weekday == 6 || date.weekday == 7) {
+                                    return false;
+                                  }
+
+                                  return true;
+                                },
+                                onChanged: (val) {
+                                  selectedDate = val;
+                                },
+                                validator: (val) {
+                                  print(val);
+                                  return null;
+                                },
+                                onSaved: (val) => print(val),
+                              ),
+                            ],
+                          );
                         }),
-                      ),
-                      Text('Select date and time'),
-                      DateTimePicker(
-                        type: DateTimePickerType.dateTimeSeparate,
-                        dateMask: 'd MMM, yyyy',
-                        initialValue: DateTime.now().toString(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100),
-                        icon: Icon(Icons.event),
-                        dateLabelText: 'Date',
-                        timeLabelText: "Hour",
-                        selectableDayPredicate: (date) {
-                          // Disable weekend days to select from the calendar
-                          if (date.weekday == 6 || date.weekday == 7) {
-                            return false;
-                          }
-
-                          return true;
-                        },
-                        onChanged: (val) {
-                          selectedDate = val;
-                        },
-                        validator: (val) {
-                          print(val);
-                          return null;
-                        },
-                        onSaved: (val) => print(val),
-                      ),
-                    ],
-                  ),
+                        error: (e, stackTrace) => Text('Error happened'),
+                        loading: () => Text('Loading...'));
+                  })),
                 );
               }),
         ));
